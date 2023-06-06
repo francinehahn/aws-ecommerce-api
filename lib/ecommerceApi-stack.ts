84806294fe9d9a8ef5505de62e463e2fddeeb901
Francine Hahn <fran_hahn@hotmail.com>
2023-06-06 16:41:36 -0300

    feat: I integrated the lambda function productsFetchFunction to the API Gateway ecommerceApiStack

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -4,12 +4,39 @@ import * as apiGateway from "aws-cdk-lib/aws-apigateway"
 import * as cwLogs from "aws-cdk-lib/aws-logs"
 import { Construct } from "constructs"
 
+interface EcommerceApiStackProps extends cdk.StackProps {
+    productsFetchHandler: lambdaNodeJS.NodejsFunction
+}
+
 export class EcommerceApiStack extends cdk.Stack {
-    constructor (scope: Construct, id: string, props?: cdk.StackProps) {
+    constructor (scope: Construct, id: string, props: EcommerceApiStackProps) {
         super (scope, id, props)
 
+        const logGroup = new cwLogs.LogGroup(this, "EcommerceApiLogs")
+
         const api = new apiGateway.RestApi(this, "EcommerceApi", {
-            restApiName: "EcommerceApi"
+            restApiName: "EcommerceApi",
+            cloudWatchRole: true,
+            deployOptions: {
+                accessLogDestination: new apiGateway.LogGroupLogDestination(logGroup),
+                accessLogFormat: apiGateway.AccessLogFormat.jsonWithStandardFields({
+                    httpMethod: true,
+                    ip: true,
+                    protocol: true,
+                    requestTime: true,
+                    resourcePath: true,
+                    responseLength: true,
+                    status: true,
+                    caller: true,
+                    user: true
+                })
+            }
         })
+
+        const productsFetchIntegration = new apiGateway.LambdaIntegration(props.productsFetchHandler)
+        
+        // "/products"
+        const productsRosource = api.root.addResource("products")
+        productsRosource.addMethod("GET", productsFetchIntegration)
     }
 }
