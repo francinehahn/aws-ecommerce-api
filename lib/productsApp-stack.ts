14cde3df12266ca33047ea5dd62554ce62378e20
Francine Hahn <fran_hahn@hotmail.com>
2023-06-08 18:57:28 -0300

    feat: I added the products admin handler function to the products stack

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -6,12 +6,13 @@ import * as dynamodb from "aws-cdk-lib/aws-dynamodb"
 
 export class ProductsAppStack extends cdk.Stack {
     readonly producstFetchHandler: lambdaNodeJS.NodejsFunction
+    readonly producstAdminHandler: lambdaNodeJS.NodejsFunction
     readonly productsdb: dynamodb.Table
-    
-    
+
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props)
 
+        //database config
         this.productsdb = new dynamodb.Table(this, "Productsdb", {
             tableName: "products",
             removalPolicy: cdk.RemovalPolicy.DESTROY, //the default is that when the stack is removed, the table is maintained
@@ -24,6 +25,7 @@ export class ProductsAppStack extends cdk.Stack {
             writeCapacity: 1 //the default is 5
         })
 
+        //lambda function config
         this.producstFetchHandler = new lambdaNodeJS.NodejsFunction(this, "ProductsFetchFunction", {
             functionName: "ProductsFetchFunction",
             entry: "lambda/products/productsFetchFunction.ts",
@@ -41,5 +43,24 @@ export class ProductsAppStack extends cdk.Stack {
 
         //this will grant access to the products fetch function to access the db to read data
         this.productsdb.grantReadData(this.producstFetchHandler)
+
+        //lambda function config
+        this.producstAdminHandler = new lambdaNodeJS.NodejsFunction(this, "ProductsAdminFunction", {
+            functionName: "ProductsAdminFunction",
+            entry: "lambda/products/productsAdminFunction.ts",
+            handler: "handler",
+            memorySize: 128,
+            timeout: cdk.Duration.seconds(5),
+            bundling: {
+                minify: true,
+                sourceMap: false
+            },
+            environment: {
+                PRODUCTS_DB: this.productsdb.tableName
+            }
+        })
+
+        //this will grant access to the products admin function to access the db to write data
+        this.productsdb.grantWriteData(this.producstAdminHandler)
     }
 }
