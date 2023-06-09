ff35d3d6920709b6a339907b0f3b7116f0da607f
Francine Hahn <fran_hahn@hotmail.com>
2023-06-09 18:46:13 -0300

    feat: I added some configs related to the productEventsLayer

--- lib/productsAppLayers-stack.ts
+++ lib/productsAppLayers-stack.ts
@@ -4,12 +4,10 @@ import * as lambda from "aws-cdk-lib/aws-lambda"
 import * as ssm from "aws-cdk-lib/aws-ssm"
 
 export class ProductsAppLayersStack extends cdk.Stack {
-    readonly productsLayers: lambda.LayerVersion
-
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super (scope, id, props)
 
-        this.productsLayers = new lambda.LayerVersion(this, "ProductsLayer", {
+        const productsLayers = new lambda.LayerVersion(this, "ProductsLayer", {
             code: lambda.Code.fromAsset("lambda/products/layers/productsLayer"),
             compatibleRuntimes: [lambda.Runtime.NODEJS_18_X],
             layerVersionName: "ProductsLayer",
@@ -19,7 +17,20 @@ export class ProductsAppLayersStack extends cdk.Stack {
         //Systems Manager: Parameter Store (it stores information so that other functions can use the layers) 
         new ssm.StringParameter(this, "ProductsLayerVersionArn", {
             parameterName: "ProductsLayerVersionArn",
-            stringValue: this.productsLayers.layerVersionArn
+            stringValue: productsLayers.layerVersionArn
+        })
+
+        const productEventsLayer = new lambda.LayerVersion(this, "ProductEventsLayer", {
+            code: lambda.Code.fromAsset("lambda/products/layers/productEventsLayer"),
+            compatibleRuntimes: [lambda.Runtime.NODEJS_18_X],
+            layerVersionName: "ProductEventsLayer",
+            removalPolicy: cdk.RemovalPolicy.RETAIN //the default pattern would be to destroy
+        })
+
+        //Systems Manager: Parameter Store (it stores information so that other functions can use the layers) 
+        new ssm.StringParameter(this, "ProductEventsLayerVersionArn", {
+            parameterName: "ProductEventsLayerVersionArn",
+            stringValue: productEventsLayer.layerVersionArn
         })
     }
 }
