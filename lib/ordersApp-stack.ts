2184b51748da7e6203230b969f96594db531add1
Francine Hahn <fran_hahn@hotmail.com>
2023-06-10 12:41:53 -0300

    feat: I included some other configs to the ordersAppStack class

--- lib/ordersApp-stack.ts
+++ lib/ordersApp-stack.ts
@@ -11,6 +11,8 @@ interface OrdersAppStackProps extends cdk.StackProps {
 }
 
 export class OrdersAppStack extends cdk.Stack {
+    readonly ordersHandler: lambdaNodeJS.NodejsFunction
+
     constructor (scope: Construct, id: string, props: OrdersAppStackProps) {
         super(scope, id, props)
 
@@ -28,5 +30,35 @@ export class OrdersAppStack extends cdk.Stack {
             readCapacity: 1,
             writeCapacity: 1
         })
+
+        //This stack will use the orders layer
+        const ordersLayerArn = ssm.StringParameter.valueForStringParameter(this, "OrdersLayerVersionArn")
+        const ordersLayer = lambda.LayerVersion.fromLayerVersionArn(this, "OrdersLayerVersionArn", ordersLayerArn)
+
+        //This stack will also use the products layer
+        const productsLayerArn = ssm.StringParameter.valueForStringParameter(this, "ProductsLayerVersionArn")
+        const productsLayer = lambda.LayerVersion.fromLayerVersionArn(this, "ProductsLayerVersionArn", productsLayerArn)
+
+        this.ordersHandler = new lambdaNodeJS.NodejsFunction(this, "OrdersFunction", {
+            functionName: "OrdersFunction",
+            entry: "lambda/orders/ordersFunction.ts",
+            handler: "handler",
+            memorySize: 128,
+            timeout: cdk.Duration.seconds(2),
+            bundling: {
+                minify: true,
+                sourceMap: false
+            },
+            environment: {
+                PRODUCTS_DB: props.productsdb.tableName,
+                ORDERS_DB: ordersdb.tableName,
+            },
+            layers: [ordersLayer, productsLayer],
+            tracing: lambda.Tracing.ACTIVE,
+            insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0 //it adds another lambda layer
+        })
+
+        ordersdb.grantReadWriteData(this.ordersHandler)
+        props.productsdb.grantReadData(this.ordersHandler)
     }
 }
