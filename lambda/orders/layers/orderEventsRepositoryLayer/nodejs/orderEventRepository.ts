2499c55bd896726e5ece24d715b0c4c8dfa5f9db
Francine Hahn <fran_hahn@hotmail.com>
2023-06-13 10:25:59 -0300

    feat: I included 2 methods in the orderEventRepository file

--- lambda/orders/layers/orderEventsRepositoryLayer/nodejs/orderEventRepository.ts
+++ lambda/orders/layers/orderEventsRepositoryLayer/nodejs/orderEventRepository.ts
@@ -30,4 +30,33 @@ export class OrderEventRepository {
             Item: orderEvent
         }).promise()
     }
+
+    //query search using the global secondary index
+    async getOrderEventsByEmail (email: string) {
+        const data = await this.dbclient.query({
+            TableName: this.eventsdb,
+            IndexName: "emailIndex",
+            KeyConditionExpression: "email = :email AND begins_with(sk, :prefix)",
+            ExpressionAttributeValues: {
+                ":email": email,
+                ":prefix": "ORDER_"
+            }
+        }).promise()
+
+        return data.Items as OrderEventDb[]
+    }
+
+    async getOrderEventsByEmailAndEventType (email: string, eventType: string) {
+        const data = await this.dbclient.query({
+            TableName: this.eventsdb,
+            IndexName: "emailIndex",
+            KeyConditionExpression: "email = :email AND begins_with(sk, :prefix)",
+            ExpressionAttributeValues: {
+                ":email": email,
+                ":prefix": eventType
+            }
+        }).promise()
+
+        return data.Items as OrderEventDb[]
+    }
 }
