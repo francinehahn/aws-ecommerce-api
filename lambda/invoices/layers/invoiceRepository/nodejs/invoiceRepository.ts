83e51c7308334cd044a375478b920e562e4ebb94
Francine Hahn <fran_hahn@hotmail.com>
2023-06-13 21:57:02 -0300

    feat: I wrote the invoiceRepository layer

--- lambda/invoices/layers/invoiceRepository/nodejs/invoiceRepository.ts
+++ lambda/invoices/layers/invoiceRepository/nodejs/invoiceRepository.ts
@@ -1,4 +1,39 @@
+import { DocumentClient } from "aws-sdk/clients/dynamodb"
+
+export interface InvoiceFile {
+    customerName: string,
+    invoiceNumber: string,
+    totalValue: number,
+    productId: string,
+    quantity: number
+}
+
 export interface Invoice {
     pk: string,
-    sk: string
+    sk: string,
+    totalValue: number,
+    productId: string,
+    quantity: number,
+    transactionId: string,
+    ttl: number,
+    createdAt: number
+}
+
+export class InvoiceRepository {
+    private dbClient: DocumentClient
+    private invoicesDb: string
+
+    constructor (dbClient: DocumentClient, invoicesDb: string) {
+        this.dbClient = dbClient
+        this.invoicesDb = invoicesDb
+    }
+
+    async createInvoice (invoice: Invoice): Promise<Invoice> {
+        await this.dbClient.put({
+            TableName: this.invoicesDb,
+            Item: invoice  
+        }).promise()
+
+        return invoice
+    }
 }
